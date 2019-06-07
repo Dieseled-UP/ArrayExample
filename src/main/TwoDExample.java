dd08c6ef041a5b68432440a7feadc7d233c7d940
Dieseled-UP <l00099023@student.lyit.ie>
2019-06-07 10:38:10 +0100

    second commit

---
+++ src/main/TwoDExample.java
@@ -0,0 +1,57 @@
+package main;
+
+public class TwoDExample {
+
+	public static void main(String[] args) {
+
+		int[][] temps = {
+				{1, -2, 3},
+				{-4, -5, 6, 9},
+				{7}
+		};
+		
+		for (int i = 0; i < temps.length; i++) {
+			
+			for (int j = 0; j < temps[i].length; j++) {
+				
+				System.out.print(temps[i][j] + ", ");
+			}
+			
+			System.out.println();
+		}
+		
+		for (int[] elements: temps) {
+			
+			for(int num: elements) {
+				
+				System.out.print(num + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
